9a767a5b58213712c89d4e1a8afb72e741f4db76
imsrpanda <imsrpanda@gmail.com>
2026-07-28 11:21:40 +0530

    changes in both table

--- Database/Tables/Employee.sql
+++ Database/Tables/Employee.sql
@@ -1,9 +1,13 @@
-CREATE TABLE dbo.Employee
-(
-    EmployeeID INT PRIMARY KEY IDENTITY(1,1),
-    FirstName NVARCHAR(100),
-    LastName NVARCHAR(100),
-    Email NVARCHAR(200),
-    Salary DECIMAL(12,2),
-    CreatedDate DATETIME DEFAULT GETDATE()
-);
+IF OBJECT_ID('dbo.Employee', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Employee
+    (
+        EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+        FirstName NVARCHAR(100),
+        LastName NVARCHAR(100),
+        Email NVARCHAR(200),
+        Salary DECIMAL(12,2),
+        CreatedDate DATETIME DEFAULT GETDATE()
+    );
+END;
+GO
--- Database/Tables/department.sql
+++ Database/Tables/department.sql
@@ -1,5 +1,8 @@
-CREATE TABLE dbo.Department
-(
-    DepartmentID INT PRIMARY KEY IDENTITY,
-    DepartmentName NVARCHAR(100)
-);
+IF OBJECT_ID('dbo.Department', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.Department
+    (
+        DepartmentID INT IDENTITY(1,1) PRIMARY KEY,
+        DepartmentName NVARCHAR(100)
+    );
+END;
