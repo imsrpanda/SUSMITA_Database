04c3d18345eab5b9663f979163aa7bc510ea6e7b
imsrpanda <imsrpanda@gmail.com>
2026-07-28 13:02:19 +0530

    status

--- Database/Tables/AlterEmployee.sql
+++ Database/Tables/AlterEmployee.sql
@@ -1,7 +1,9 @@
+IF COL_LENGTH('dbo.Employee', 'PhoneNumber') IS NULL
+BEGIN
 ALTER TABLE dbo.Employee
 ADD PhoneNumber VARCHAR(20);
 
 ALTER TABLE dbo.Employee
 ALTER COLUMN Salary DECIMAL(18,2);
-
+END;
 Go
