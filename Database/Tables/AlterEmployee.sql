a30aa9d594f5fa5d3ea37dca4eae21e3764ff500
imsrpanda <imsrpanda@gmail.com>
2026-07-28 13:44:42 +0530

    alter

--- Database/Tables/AlterEmployee.sql
+++ Database/Tables/AlterEmployee.sql
@@ -1,10 +1,5 @@
-IF COL_LENGTH('dbo.Employee', 'salary') IS NULL
-BEGIN
-ALTER TABLE dbo.Employee
-ADD PhoneNumber VARCHAR(20);
+
 
 ALTER TABLE dbo.Employee
 ALTER COLUMN Salary DECIMAL(18,2);
 
-END;
-Go
